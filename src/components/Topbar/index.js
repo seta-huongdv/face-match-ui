0bbf874f8869844a48448c943b6406cc6584207d
datlt6709 <datlt6709@seta-international.vn>
2018-05-28 17:27:49 +0700

    update UI

--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { appBarHeight } from '../AppBar';
 import styles from './styles.scss';
 import {TopBar as LibTopBar} from 'veritone-react-common';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
+import Paper from '@material-ui/core/Paper';
+
+import {
+  string,
+  bool,
+  func,
+  arrayOf,
+  element,
+  number,
+  shape
+} from 'prop-types';
 
 export default class TopBar extends React.Component {
-  static propTypes = {};
-  static defaultProps = {};
+  static propTypes = {
+    appBarOffset: bool,
+    leftOffset: number,
+    leftText: string
+  };
+  static defaultProps = {
+    leftOffset: 0
+  };
 
   render() {
       return (
@@ -17,17 +35,21 @@ export default class TopBar extends React.Component {
                       <Button variant="raised" color="primary">New Case</Button>
                   </div>
               </div>
-              <LibTopBar
-                       appBarOffset
-                       sideBarOffset
-                       leftOffset={240}
-                       leftText="Newport Insland Theft"
-                       rightIconButtons={[
-                           <IconButton key="1">
-                               <InfoIcon/>
-                           </IconButton>,
-                       ]}
-              />
+              <Paper
+                  style={{
+                    top: 60,
+                    height: 60,
+                    background: '#fafafa'
+                  }}
+                  className={styles.topBar}
+                >
+                <div className={styles.rightContainer}>
+                    <span>
+                      {/*{this.props.leftText}*/} Title Case
+                    </span>
+                </div>
+              </Paper>
+
           </React.Fragment>
       );
   }
