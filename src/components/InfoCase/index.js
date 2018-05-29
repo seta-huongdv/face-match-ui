89dde312bbb7bc5d06c19be39ec52c5f43c8f4be
datlt6709 <datlt6709@seta-international.vn>
2018-05-29 17:34:00 +0700

    update UI

--- src/components/InfoCase/index.js
+++ src/components/InfoCase/index.js
@@ -2,10 +2,25 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 import styles from './styles.scss';
 
+
 export default class InfoCase extends React.Component{
+  state = {
+    anchorEl: null,
+  };
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
   render(){
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
       return(
           <div className={styles.infoCaseWrap}>
               <Typography component="div" className={styles.infoCaseItem}>
@@ -28,9 +43,32 @@ export default class InfoCase extends React.Component{
                   <Typography component="span" className={styles.infoCaseContent}>J.Williams</Typography>
               </Typography>
               <Typography component="div" className={styles.infoCaseItem}>
-                  <IconButton className={styles.cardHeaderBtn}>
+                  <IconButton
+                      className={styles.cardHeaderBtn}
+                      onClick={this.handleMenu}
+                    >
                       <MoreVertIcon/>
                   </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}>Share All Case Details</MenuItem>
+                  <MenuItem onClick={this.handleClose}>Edit Case</MenuItem>
+                  <MenuItem onClick={this.handleClose}>Close Case</MenuItem>
+                  <MenuItem onClick={this.handleClose}>Archive Case</MenuItem>
+                </Menu>
+
               </Typography>
           </div>
         )
