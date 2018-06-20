861543bf6d3042a593703e7a9d630b01dc8c7fe1
datlt6709 <datlt6709@seta-international.vn>
2018-06-20 15:20:18 +0700

    update ui

--- src/components/ProcessedSidebar/index.js
+++ src/components/ProcessedSidebar/index.js
@@ -6,6 +6,10 @@ import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import MenuItem from '@material-ui/core/MenuItem';
+import Slider from '@material-ui/lab/Slider';
+import Select from '@material-ui/core/Select';
 import styles from './styles.scss';
 
 function TabContainer(props) {
@@ -19,14 +23,17 @@ function TabContainer(props) {
 export default class ProcessedSidebar extends React.Component {
   state = {
     value: 'one',
+    range: 30,
   };
 
   handleChange = (event, value) => {
     this.setState({ value });
   };
+  handleChangeSlider = (event, range) => this.setState({ range });
 
   render() {
     const { value } = this.state;
+    const { range } = this.state;
 
     return (
       <div className={styles.processedSidebar}>
@@ -66,9 +73,32 @@ export default class ProcessedSidebar extends React.Component {
                   </FormGroup>
                 </FormControl>
               </div>
-              <div className={styles.itemFilter}>
-                <FormControl component="fieldset">
+              <div className={styles.itemFilter} >
+                <FormControl component="fieldset" style={{width: '100%'}}>
                   <FormLabel component="legend" className={styles.itemFilterLabel}>Age</FormLabel>
+                  <Slider value={range} aria-labelledby="label" onChange={this.handleChangeSlider} />
+                  <Grid container>
+                    <Grid item xs="6">
+                      <Select>
+                        <MenuItem value="" disabled>
+                          ---
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </Grid>
+                    <Grid item xs="6" style={{textAlign: 'right'}}>
+                      <Select>
+                        <MenuItem value="" disabled>
+                          ---
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </Grid>
+                  </Grid>
                 </FormControl>
               </div>
               <div className={styles.itemFilter}>
@@ -134,9 +164,32 @@ export default class ProcessedSidebar extends React.Component {
                   </FormGroup>
                 </FormControl>
               </div>
-              <div className={styles.itemFilter}>
-                <FormControl component="fieldset">
+              <div className={styles.itemFilter} >
+                <FormControl component="fieldset" style={{width: '100%'}}>
                   <FormLabel component="legend" className={styles.itemFilterLabel}>Height</FormLabel>
+                  <Slider value={range} aria-labelledby="label" onChange={this.handleChangeSlider} />
+                  <Grid container>
+                    <Grid item xs="6">
+                      <Select>
+                        <MenuItem value="" disabled>
+                          ---
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </Grid>
+                    <Grid item xs="6" style={{textAlign: 'right'}}>
+                      <Select>
+                        <MenuItem value="" disabled>
+                          ---
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                      </Select>
+                    </Grid>
+                  </Grid>
                 </FormControl>
               </div>
               <div className={styles.itemFilter}>
